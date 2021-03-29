4a2bb82b59fc787f08b946baa4f729f4654089a8
Gustavosouzadev <gustavoduti2003@gmail.com>
2021-03-29 01:45:44 -0300

    Finalizando Soma2Numeros

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            double X, Y, soma;
+
+            Console.WriteLine("Cálculo da soma entre dois números.");
+            Console.WriteLine("");
+            Console.Write("Digite o primeiro numero: ");
+            X = Convert.ToDouble (Console.ReadLine());
+
+            Console.Write("Digite o segundo numero: ");
+            Y = Convert.ToDouble (Console.ReadLine());
+
+            soma = X + Y;
+
+            Console.WriteLine("");
+            Console.WriteLine($"Soma: {soma}");
+        }
+    }
+}
